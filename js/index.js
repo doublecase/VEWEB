a2f8adce9bdef4fd77ca02bc4f16829847053417
doublecase <77269092+doublecase@users.noreply.github.com>
2021-03-19 12:05:47 +0900

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,53 @@
+$(".article1 .slide-group").slick({
+    autoplay: true, // 자동재생
+    autoplaySpeed: 3000, // 간격시간
+    dots: true, // 동그라미버튼
+    speed: 600, // 바뀌는시간(생략가능)
+    slidesToShow: 1, // 보여질슬라이드수(생략가능)
+    slidesToScroll: 1, // 이동슬라이드수(생략가능)
+    pauseOnHover: false, // 마우스오버시 멈춤여부(생략가능)
+    pauseOnDotsHover: false, // 동그라미번호버튼에 호버시 멈춤여부(생략가능)
+    pauseOnFocus: false, // 동그라미번호버튼 클릭시 자동실행 멈춤여부
+    cssEase: 'linear', // 속도함수(생략가능)
+    draggable: true, // 마우스드래그시 슬라이드 교체가능여부(생략가능)
+    fade: false, // 슬라이드가 수평으로 이동하지 않고, 제자리에서 사라지고 나타남(생략가능)
+    arrows: false, // 좌우화살표 사용여부(생략가능)
+    prevArrow: '<button class="prev"><i class="fas fa-angle-left"></i></button>',
+    nextArrow: '<button class="next"><i class="fas fa-angle-right"></i></button>',
+})
+
+
+$(".article6 .swiper").slick({
+    autoplay: false, // 자동재생
+    autoplaySpeed: 3000, // 간격시간
+    dots: false, // 동그라미버튼
+    speed: 600, // 바뀌는시간(생략가능)
+    slidesToShow: 1, // 보여질슬라이드수(생략가능)
+    slidesToScroll: 1, // 이동슬라이드수(생략가능)
+    pauseOnHover: false, // 마우스오버시 멈춤여부(생략가능)
+    pauseOnDotsHover: false, // 동그라미번호버튼에 호버시 멈춤여부(생략가능)
+    pauseOnFocus: false, // 동그라미번호버튼 클릭시 자동실행 멈춤여부
+    cssEase: 'linear', // 속도함수(생략가능)
+    draggable: true, // 마우스드래그시 슬라이드 교체가능여부(생략가능)
+    fade: true, // 슬라이드가 수평으로 이동하지 않고, 제자리에서 사라지고 나타남(생략가능)
+    arrows: true, // 좌우화살표 사용여부(생략가능)
+    prevArrow: '<button class="prev"><i class="fas fa-angle-left"></i></button>',
+    nextArrow: '<button class="next"><i class="fas fa-angle-right"></i></button>',
+})
+
+
+var header = $('header');
+var logo = $('.logo');
+$(window).scroll(function(){
+    if($(this).scrollTop() > 0) {
+        header.addClass('on');
+    }  else {
+        header.removeClass('on');
+    }
+})
+
+var tabImg = $('.tabimg');
+$('.article3 .tab').eq(1).on('click',function(){
+    tabImg.addClass('on')
+    // tabImg.eq(0).removeClass('on')
+})
